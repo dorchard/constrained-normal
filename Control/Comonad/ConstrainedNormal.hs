58ab0377fa1ce25aed8a530a67c1682fc33aae86
Dominic Orchard <dom.orchard@gmail.com>
2013-05-21 11:35:13 +0100

    completed normalised comonads - needs some refining

--- Control/Comonad/ConstrainedNormal.hs
+++ Control/Comonad/ConstrainedNormal.hs
@@ -1,113 +1,50 @@
 {-# LANGUAGE InstanceSigs, KindSignatures, GADTs, RankNTypes, ConstraintKinds, ScopedTypeVariables, FlexibleInstances #-}
 
-{-# LANGUAGE MultiParamTypeClasses #-}
-
--- |
--- Module: Control.Monad.ConstrainedNormal
--- Copyright: (c) 2013 The University of Kansas
--- License: BSD3
---
--- Maintainer: Neil Sculthorpe <neil@ittc.ku.edu>
--- Stability: alpha
--- Portability: ghc
---
--- This module provides constrained normalised type classes.  The ideas behind this module are documented in the following paper:
---
---   /The Constrained-Monad Problem/.  Neil Sculthorpe and Jan Bracker and George Giorgidze and Andy Gill.  2013. <http://www.ittc.ku.edu/~neil/papers_and_talks/constrained-monad-problem.pdf>
-
 module Control.Comonad.ConstrainedNormal
-  ( -- * Constrained Normalised Comonads
-    NC(..)--, liftNC, lowerNC, foldNC,
-  )
+ -- ( -- * Constrained Normalised Comonads
+ --   NC(..)--, liftNC, lowerNC, foldNC,
+ -- )
 where
 
 import GHC.Exts (Constraint)
 
 import Control.Comonad
-import Control.Category hiding ((.))
-import qualified Control.Category as Category
-
+-- import Control.Category.ConstrainedNormal
 
 -------------------------------------------------------------------------------------------------
 
---data CoKleisli :: (* -> Constraint) -> (* -> *) -> * -> * -> * where
---    CoKleisli :: (NC c d a -> b) -> CoKleisli c d a b
---    Extract :: (d a -> a) -> CoKleisli c d a a
+data CoK1 :: (* -> Constraint) -> (* -> *) -> * -> * -> * where
+    CoK1 :: (NCC c d a -> b) -> CoK1 c d a b
+    Extract1 :: CoK1 c d a a
 
---unCoKleisli :: CoKleisli c d a b -> (NC c d a -> b)
----unCoKleisli (Extract k) = k
---unCoKleisli (CoKleisli k) = k
+data NC1 :: (* -> Constraint) -> (* -> *) -> * -> * where
+    --Counit1 :: c x => x -> NC1 c d x 
+    Cobind1 :: c x => (CoK1 c d x y) -> d x -> NC1 c d y
 
+data NCC :: (* -> Constraint) -> (* -> *) -> * -> * where
+    NCC :: NC1 c d x -> (forall w . c w => d w -> w) -> NCC c d x
 
---class Lower a c x z where
---    lower :: a z z -> (forall x y . Lower a c y z => a y z -> a x y -> a x z) -> NA c a x z -> a x z
---    lower unit comp (Comp g f) = comp (lower unit comp g) f
+liftNCo :: c x => (forall w . c w => d w -> w) -> d x -> NCC c d x
+liftNCo counit dx = NCC (Cobind1 Extract1 dx) counit
+                    --NCC (Counit1 (counit dx)) counit
 
---    lower unit comp Unit = unit
+lowerNCo :: forall a c d . (forall x . c x => (d x -> a) -> d x -> d a) -> NCC c d a -> d a
+lowerNCo ext = lowerNC' where lowerNC' :: NCC c d a -> d a
+                              lowerNC' (NCC (Cobind1 (CoK1 k) dx) counit) = ext (k . (liftNCo counit)) dx
 
---instance Lower a c x z where
+instance Functor (NCC c d) where
+    fmap = liftW
 
+instance Comonad (NCC c d) where
+    --extract (NCC (Counit1 x) _) = x
 
-data NC :: (* -> Constraint) -> (* -> *) -> * -> * where
-    -- Extract :: (NC c d a -> a, NC c d a) -> NC c d a
-    Extract :: d a -> NC c d a
-    -- Ext :: CoKleisli c d a a -> NC c d a a
-    -- Extt :: a -> NC c d a a
-    --Extend :: c a => (CoKleisli c d a b) -> d a -> NC c d b
-    --Extend :: c a => (NC c d a -> b) -> d a -> NC c d b
-    Extend :: c x => (NC c d x -> y) -> d x -> NC c d y
-    -- Extend :: c x => (d x -> y) -> NC c d x -> NC c d y
-
-instance Functor (NC c t) where
-  fmap :: (a -> b) -> NC c t a -> NC c t b
-  fmap = liftW
-
-instance Comonad (NC c t) where
-  --extract :: NC c t a -> a
-  --extract = 
-
-  extend :: (NC c t a -> b) -> NC c t a -> NC c t b
-  -- extend k (Extract a)   = Extract (k a)                   -- right-identity law
-  --extend k (Extend h da) = Extend (CoKleisli $ \a -> k (extend (unCoKleisli h) a)) da -- associativity law
-  -- extend k (Extend h da) = Extend (\a -> k (extend h a)) da -- associativity law
-  --extend k (Extend h da) = Extend (\a -> k (extend h a)) da -- associativity law
- 
-
---dextend :: (NC c t a -> b) -> NC c t a -> NC c t b
---dextend k (Extend h da) = Extend da undefined
-
--- Extend da (\a -> k (extend h a)) -- associativity law
-
---liftNC :: c a => (d a -> a) -> d a -> NC c d a
---liftNC ext da = Extend (Extract ext) da -- right-identity law
-
---liftNC :: c a => (d a -> a) -> d a -> NC c d a
---liftNC counit da = Extend counit (Extract da) -- right-identity law
-
-
---foldNC :: forall a c r d . (r -> a) -> (forall x. c a => (r -> a) -> r -> d a) -> r -> NC c d a  
---foldNC coret cobind = foldNC'
---                        where
---                          foldNC' :: r -> NC c d a
-                          -- foldNC' (Extract da) = coret da
---                          foldNC' (Extend k dx)  = extend k (foldNC' dx)
-
---lowerNM :: forall a c t. (d a -> a) -> (forall x. c x => (d x -> a) -> d x -> d a) -> d a -> NC c d a
---lowerNM coret cobind = foldNM'
---                           where
---                             foldNM' :: NM c t a -> r
---                             foldNM' (Return a)   = ret a
---                             foldNM' (Bind tx k)  = bind tx (foldNM' . k)
-
-
-lowerNC :: forall a c d . (forall x . c x => (d x -> a) -> d x -> d a) -> NC c d a -> d a
-lowerNC = undefined
-
---lowerNM :: forall a c t. (a -> t a) -> (forall x. c x => t x -> (x -> t a) -> t a) -> NM c t a -> t a
---lowerNM = foldNM
-
---data CY (c :: * -> Constraint) (d :: * -> *) (a :: *) where CY :: c r => ((d a -> r) -> d r) -> CY c d a
-
---liftCY :: (forall r . c r => (d a -> r) -> d a -> d r) -> d a -> CY c d a
---liftCY cbind da = CY (\k -> cbind k da)
+    extract (NCC (Cobind1 (CoK1 k) dx) counit) = k (liftNCo counit dx)
+    extract (NCC (Cobind1 (Extract1) dx) counit) = counit dx
     
+    --extend k (NCC (Counit1 x) counit) = NCC (Cobind1 (CoK1 k) ) counit
+
+    extend k (NCC (Cobind1 Extract1 dx) counit) = NCC (Cobind1 (CoK1 k) dx) counit
+    extend k (NCC (Cobind1 (CoK1 g) dx) counit) = 
+         let w = Cobind1 (CoK1 $ k . extend g) dx -- undefined -- (k ((extend g) (liftNC1 counit dx)))
+         in NCC w counit 
+
